28be3543df858bc0c3aeb4f46095c6723de643b6
shakeway <fishgo1234@gmail.com>
2017-04-18 22:17:08 -0700

    up

--- easy_web_crawler.py
+++ easy_web_crawler.py
@@ -2,18 +2,27 @@ from urllib.request import urlopen
 import re
 
 def crawl(url, maxurls):
+	"""Starting from this URL, crawl the web until
+    you have collected maxurls URLS, then return them
+    as a set"""
 	urls = set([url])
 	while(len(urls) < maxurls):
+		# remove a URL at random
 		url = urls.pop()
 		print("URL: ", url)
 		links = get_links(url)
 		urls.update(links)
+		# add the url back to the set
 		urls.add(url)
 
 	return urls
 
 def get_links(url):
+	"""Scan the text for http URLs and return a set
+    of URLs found, without duplicates"""
 	text = get_page(url)
+
+	# look for any http URL in the page
 	links = set()
 	urlpattern = r"(https?\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/[^<'\";]*)?)"
 
@@ -26,6 +35,9 @@ def get_links(url):
 
 
 def get_page(url):
+	"""Get the text of the web page at the given URL
+    return a string containing the content"""
+    
 	fd = urlopen(url)
 	content = fd.read()
 	fd.close()
